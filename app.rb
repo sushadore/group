1849cf178cdec8a9ecc2c87de0039175edf0d740
Monique St. Laurent and Sowmya Dinavahi <sowmya@>
2017-05-16 13:18:19 -0700

    update app.rb and views files

--- app.rb
+++ app.rb
@@ -10,31 +10,47 @@ get "/" do
   erb :index
 end
 
-get "/producers" do
+get "/producer/signin" do
   erb :producer_login
 end
 
-# post "/producer/signup" do
-#   attendee = Attendee.new(:username => params[:username], :password => params[:password])
-#   if attendee.save
-#     puts "Success!"
-#     redirect back
-#   else
-#     puts "Failure."
-#     erb :security
-#   end
-# end
 
-post "/attendee/signup" do
-  attendee = Attendee.new(:name => params[:name], :username => params[:username], :password => params[:password])
-  if attendee.save
-    redirect back
+
+get "/producer/signup" do
+  erb :producer_create_account
+end
+
+
+
+post "/producer/signup" do
+  producer_name = params['name']
+  producer_username = params['username']
+  producer_password = params['password']
+  @producer = Producer.create(:name => producer_name, :username => producer_username, :password => producer_password, :id => nil)
+  if @producer.save
+    erb :producer_navigation
   else
     erb :security
   end
 end
 
-post "/attendee/login" do
+post "/attendee/signup" do
+  attendee_name = params['name']
+  attendee_username = params['username']
+  attendee_password = params['password']
+  @attendee = Attendee.create(:name => attendee_name, :username => attendee_username, :password => attendee_password, :id => nil)
+  if @attendee.save
+    erb :attendee
+  else
+    erb :security
+  end
+end
+
+get "/attendee/signin" do
+  erb :attendee_login
+end
+
+post "/attendee/signin" do
   attendee = Attendee.find_by(:username => params[:username])
   if attendee && attendee.authenticate(params[:password])
     session[:user_id] = attendee.id
@@ -44,6 +60,7 @@ post "/attendee/login" do
   end
 end
 
+# end
 get "/attendee/:id" do
   if session[:username] !=nil
     @artist.all
