c2a7dcba9484a24abcdb17cc9af6bd99779a1c0f
Monique St Laurent and Susha Dore <susha@>
2017-05-17 12:06:52 -0700

    add functionality for attendee to add,view and delete artists

--- app.rb
+++ app.rb
@@ -14,10 +14,9 @@ end
 
 post "/attendee/signup" do
   attendee_name = params['name']
-
   attendee_username = params['username']
   attendee_password = params['password']
-  @artist = Artist.all
+  @artists = Artist.all
   @attendee = Attendee.create(:name => attendee_name, :username => attendee_username, :password => attendee_password, :id => nil)
   if @attendee.save
     erb :attendee
@@ -27,13 +26,12 @@ post "/attendee/signup" do
 end
 
 get "/attendee/signin" do
-
   erb :attendee_login
 end
 
 post "/attendee/success" do
   @attendee = Attendee.find_by(:username => params.fetch('username'))
-
+  @artists = Artist.all
   if @attendee.password == params.fetch('password')
     session[:user_id] = @attendee.id
     erb :attendee
@@ -42,20 +40,48 @@ post "/attendee/success" do
   end
 end
 
-# end
 get "/attendee/:id" do
   id = params.fetch('id')
   @attendee = Attendee.find(id)
-  if session[:username] !=nil
-    @artist.all
-    erb :attendee
-  else
-    erb :security
-  end
+  @attendee_artist = @attendee.artists
+  @artists = Artist.all - @attendee_artist
+  erb :attendee
 end
 
+post "/attendee/:id/add_artist" do
+  @attendee = Attendee.find(params['id'])
+  @artist = Artist.find(params['artists_id'])
+  @attendee.artists.push(@artist)
+  @artists = Artist.all
+  redirect "/attendee/#{@attendee.id}"
+end
 
+delete "/attendee/:id/remove_artist" do
+  @attendee = Attendee.find(params['id'])
+  artist_id = params["artist_id"]
+  @attendee.artists.destroy(Artist.find(artist_id))
+  redirect "/attendee/#{@attendee.id}"
+end
 
+get "/artist/:id" do
+  id = params.fetch('id')
+  @artist = Artist.find(id)
+  erb :artist
+end
+
+get('/attendee/:id/remove_artist/:artist_id') do |attendee_id, artist_id|
+  attendee = Attendee.find(attendee_id)
+  artist = Artist.find(artist_id)
+  attendee.artists.destroy(artist)
+  redirect to("/attendee/#{attendee_id}")
+end
+# post "/attendee/add-review" do
+#   @artist = Artist.find(params['artists_id'])
+#   @attendee.artists.push(@artist)
+#   artist_review = params.fetch('user_reviews')
+#   @artist = Artist.find(artist_review)
+#   redirect back
+# end
 # ******** PRODUCER/ADMIN SIDE ********
 
 get "/producer/signin" do
@@ -90,7 +116,8 @@ post "/producer" do
 end
 
 get "/producer/:id" do
-  @producer = Producer.find(params.fetch('id'))
+  id = params.fetch('id')
+  @producer = Producer.find(id)
   @stages = Stage.all
   @artists = Artist.all
   erb :producer
